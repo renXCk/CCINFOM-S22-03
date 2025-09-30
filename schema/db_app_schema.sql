d99b080fcd522afeee596a98ba01c52611b781c2
Ren <aaron.gonzales012808@gmail.com>
2025-09-30 21:25:19 +0800

    Fixed Formatting for Vehicle

--- schema/db_app_schema.sql
+++ schema/db_app_schema.sql
@@ -9,12 +9,12 @@
 
 -- Vehicle (Ren)
 CREATE TABLE Vehicle (
-    vehicle_id INT AUTO_INCREMENT PRIMARY KEY,
-    plate_number VARCHAR(20) NOT NULL UNIQUE,
-    vehicle_type VARCHAR(50) NOT NULL,
-    model VARCHAR(50) NOT NULL,
-    status ENUM('available','on_trip','maintenance','inactive') DEFAULT 'available',
-    mileage INT DEFAULT 0
+    vehicle_id 		INT AUTO_INCREMENT PRIMARY KEY,
+    plate_number 	VARCHAR(20) NOT NULL UNIQUE,
+    vehicle_type 	VARCHAR(50) NOT NULL,
+    model 			VARCHAR(50) NOT NULL,
+    status 			ENUM('available','on_trip','maintenance','inactive') DEFAULT 'available',
+    mileage 		INT DEFAULT 0
 );
 
 -- Client (Ken)
