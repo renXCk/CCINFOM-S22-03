830bf471886fff0f88902feca43d9506bfea60e8
natzbtw <leelancze1111@gmail.com>
2025-11-03 16:15:46 +0800

    swing gui test

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
+import jdk.jfr.Percentage;
+import util.DBConnection;
+
+import javax.swing.*;
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Main {
-        
+    public static void main(String[] args){
+        DBView view = new DBView();
+        DBController cont = new DBController(view);
+    }
 }
--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -5,21 +5,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-        private static final String url = "jdbc:mysql://localhost:3306/dbshipment";
+        private static final String url = "jdbc:mysql://localhost:3306/deliveryshipment";
         private static final String user = "root";
         private static final String password = "password";
+        public static Connection conn;
 
         public static Connection getConnection() throws SQLException{
-                return DriverManager.getConnection(url, user, password);
+            return DriverManager.getConnection(url, user, password);
         }
 
-        public static void main(String[] args) {
-                try (Connection conn = getConnection()) {
-                        System.out.println("Database connected successfully!");
-                } catch (SQLException e) {
-                        System.out.println("Connection failed!");
-                        e.printStackTrace();
-                }
+        public DBConnection() {
+            try {
+                conn = getConnection();
+                System.out.println("Database connected successfully!");
+            } catch (SQLException e) {
+                System.out.println("Connection failed!");
+                e.printStackTrace();
+            }
         }
 
 }
