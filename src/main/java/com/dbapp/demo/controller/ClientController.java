16637ff7f4a17d722b4f0b26873c9096b0ff7d52
roni <kennese_manalang@dlsu.edu.ph>
2025-11-17 02:29:07 +0800

    Create ClientController with annotations

--- src/main/java/com/dbapp/demo/controller/ClientController.java
+++ src/main/java/com/dbapp/demo/controller/ClientController.java
@@ -1,11 +1,48 @@
 package com.dbapp.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dbapp.demo.model.Client;
+import com.dbapp.demo.services.ClientService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/clients")
 public class ClientController {
 
+    public ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    // read all clients
+    @GetMapping("/all")
+    public List<Client> getAllClients(){
+        return clientService.getAllClients();
+    }
+
+    // get client indiv by id
+    @GetMapping("/{id}")
+    public Client getClientById(@PathVariable int id){
+        return clientService.getClientById(id);
+    }
+
+    // create new client regis
+    @PostMapping("/add")
+    public boolean createClient(@RequestBody Client client) {
+        return clientService.addClient(client);
+    }
+
+    // update client info
+    @PutMapping("/update")
+    public boolean updateClient(@RequestBody Client client) {
+        return clientService.updateClient(client);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public boolean deleteClient(@PathVariable int id) {
+        return clientService.deleteClient(id);
+    }
 
 }
