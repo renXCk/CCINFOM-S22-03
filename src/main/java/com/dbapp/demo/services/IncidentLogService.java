921a8272e6e2647698a77ea5c8fdfc1d538fef44
natzbtw <leelancze1111@gmail.com>
2025-11-17 11:36:42 +0800

    incident log DAO changes

---
+++ src/main/java/com/dbapp/demo/services/IncidentLogService.java
@@ -0,0 +1,93 @@
+package com.dbapp.demo.services;
+
+import com.dbapp.demo.model.IncidentLog;
+import com.dbapp.demo.model.Driver;
+import com.dbapp.demo.model.Vehicle;
+import com.dbapp.demo.dao.DriverDAO;
+import com.dbapp.demo.dao.VehicleDAO;
+import com.dbapp.demo.dao.IncidentLogDAO;
+
+import java.util.List;
+import java.sql.Timestamp;
+
+public class IncidentLogService {
+    private final IncidentLogDAO incidentLogDAO = new IncidentLogDAO();
+    private final VehicleDAO vehicleDAO = new VehicleDAO();
+    private final DriverDAO driverDAO = new DriverDAO();
+
+    public boolean addIncidentLog(IncidentLog i) {
+        Vehicle vehicle = vehicleDAO.getVehicleById(i.getVehicleId());
+        if (vehicle == null) {
+            System.err.println("Vehicle does not exist");
+            return false;
+        }
+
+        String vehicleStatus = vehicle.getStatus();
+        if (!vehicleStatus.equals("on_trip")) {
+            System.err.println("Vehicle status must be 'on_trip'. Current status: " + vehicleStatus);
+            return false;
+        }
+
+        Driver driver = driverDAO.getDriverById(i.getDriverId());
+        if (vehicle == null) {
+            System.err.println("Vehicle does not exist");
+            return false;
+        }
+
+        String driverStatus = driver.getStatus();
+        if (!driverStatus.equals("active")) {
+            System.err.println("Driver status must be 'active'. Current status: " + driverStatus);
+            return false;
+        }
+
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        if (i.getIncidentDateTime().after(now)) {
+            System.err.println("Incident date cannot be in the future");
+            return false;
+        }
+
+        return incidentLogDAO.createIncidentLog(i);
+    }
+
+    public List<IncidentLog> getAllIncidents() {
+        return incidentLogDAO.readIncidentLogs();
+    }
+
+    public boolean updateIncidentLog(IncidentLog i) {
+        Vehicle vehicle = vehicleDAO.getVehicleById(i.getVehicleId());
+        if (vehicle == null) {
+            System.err.println("Vehicle does not exist");
+            return false;
+        }
+
+        String vehicleStatus = vehicle.getStatus();
+        if (!vehicleStatus.equals("on_trip")) {
+            System.err.println("Vehicle status must be 'on_trip'. Current status: " + vehicleStatus);
+            return false;
+        }
+
+        Driver driver = driverDAO.getDriverById(i.getDriverId());
+        if (driver == null) {
+            System.err.println("Vehicle does not exist");
+            return false;
+        }
+
+        String driverStatus = driver.getStatus();
+        if (!driverStatus.equals("active")) {
+            System.err.println("Driver status must be 'active'. Current status: " + vehicleStatus);
+            return false;
+        }
+
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        if (i.getIncidentDateTime().after(now)) {
+            System.err.println("Incident date cannot be in the future");
+            return false;
+        }
+
+        return incidentLogDAO.updateIncidentLog(i);
+    }
+
+    public boolean deleteIncidentLog(int id) {
+        return incidentLogDAO.deleteIncidentLog(id);
+    }
+}
